628c394a06b048d7ead61a0f62d209c3f71cd2cb
Josue Nery <josuepontes2010@hotmail.com>
2022-04-22 20:33:01 -0300

    React Hooks

---
+++ SpreadBootcamp/vquartoTeste/react-hooks/hooks/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import Counter from '../Counter/Counter';
+// import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../ifoodcounter/ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <Ifoodcounter />
+    </>
+  );
+}
+
+export default App;
